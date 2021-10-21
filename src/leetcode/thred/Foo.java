4ee2b28d92b75342bb85c5ab950b94b37c3e4b87
lizhongyang <123698745>
2021-10-21 18:10:45 +0800

    按序打印

---
+++ src/leetcode/thred/Foo.java
@@ -0,0 +1,43 @@
+package leetcode.thred;
+
+import java.util.concurrent.*;
+
+/**
+ * Title : leetcode.thred <br>
+ * Company : 北京华宇元典信息服务有限公司 <br>
+ * Description :
+ *
+ * @author lizhongyang
+ * @version 1.0
+ * @date 2021-10-21 16:03
+ */
+public class Foo {
+
+    Semaphore two = new Semaphore(0);
+    Semaphore three = new Semaphore(0);
+
+    public Foo() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        two.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        two.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        three.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        three.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+
+}
