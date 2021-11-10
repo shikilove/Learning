a48842935dbc6e5f52586df5fddb6bd80b0ccfec
lizhongyang <123698745>
2021-11-10 17:53:19 +0800

    Top100--5. 最长回文子串

---
+++ src/leetcode/top100/LongestPalindrome.java
@@ -0,0 +1,45 @@
+package leetcode.top100;
+
+/**
+ * Title : leetcode.top100 <br>
+ * Company : 北京华宇元典信息服务有限公司 <br>
+ * Description :
+ *
+ * @author lizhongyang
+ * @version 1.0
+ * @date 2021-11-10 17:23
+ */
+public class LongestPalindrome {
+
+    /**
+     * 给你一个字符串 s，找到 s 中最长的回文子串。
+     */
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = roundCenter(s, i, i);
+            int len2 = roundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int roundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LongestPalindrome().longestPalindrome("abbac"));
+    }
+}
